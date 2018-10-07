d394be08cd7512104cc03252cef9a330b948256b
Amine L'ch <aminelch95@gmail.com>
2018-10-07 23:14:32 +0100

    modifié le fichier examen.js

--- web/tp1/examen.js
+++ web/tp1/examen.js
@@ -1,39 +1,53 @@
 // déclaration des variables 
 
-    let nom = document.querySelector("#nom")
-    let prenom= document.querySelector("#prenom")
-    let message =document.querySelector("#resultat-message")
-    //message.innerText="Je suis un test"
-    
-    
-    
-    // cette fonction permet de verifier l'ensmeble de champs 
-function correction() { 
-    //si le champs nom | prenom est vide 
-    if(nom.value.length==0 ||prenom.value.length==0 ){
-     alert("Nom ou prenom vide , veuillez remplir tous les champs ")
-     nom.focus() 
-        
-     let radiobutton1=document.querySelector("#r1")
-     let elementCoche=false; 
-     for(var i=0;i<=radiobutton1.length;i++ ){
-         if(radiobutton1.item(0).checked==true){
-             console.log(i)
-            elementCoche=true;
-         }
-     }
-     console.log(elementCoche)
+var nom = document.querySelector("#nom");
+var prenom = document.querySelector("#prenom");
+var message = document.querySelector("#resultat-message");
+var radioEelement1 = document.querySelector("#r1");
+var radioEelement2 = document.querySelector("#r2");
+//message.innerText="Je suis un test"
+
+//Verifie si un champ radio est bien remplie ou nn 
+function radioValidation(radioElem){
+    var raValue = false;
+    for(var i=0; i<radioElem.length;i++){
+        if(radioElem[i].checked == true){
+            raValue = true;    
+        }
+    }
+    if(!raValue){
+        alert("Veuillez choisir un option");
+        return false;
+    }
+
 }
-// if()
+
+
+// cette fonction permet de verifier l'ensmeble de champs 
+function correction() {
+    
+    // si le champs nom | prenom est vide 
+    if (nom.value.length == 0 || prenom.value.length == 0) {
+        alert("Nom ou prenom vide , veuillez remplir tous les champs ")
+        nom.focus()
+
+    }
+
+    radioValidation(radioEelement1);
+    radioValidation(radioEelement2);
+
+    
+
 }
+
+
 
 
 // Au soummission de formulaire 
-document.getElementById('formulaire').addEventListener('submit', function(e){
+document.getElementById('formulaire').addEventListener('submit', function (e) {
     //on annulle le soummission de formulaire 
     e.preventDefault();
-    //on appel au fonction correction 
-    correction() 
 
+    correction();
 
 })
