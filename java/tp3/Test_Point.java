aba85720ef31c7a48ddd2e6bd58a2a14e8988f43
aminelch <aminelch95@gmail.com>
2018-10-17 21:51:24 +0100

    Ajouter de test pour le classe Point.java

--- java/tp3/Test_Point.java
+++ java/tp3/Test_Point.java
@@ -46,13 +46,19 @@ public class Test_Point {
         System.out.println("son ordonné est : " + z);
         System.out.println("\n --------------------------- \n");
         
-        //Ce code va generer un erreur , car on tente d'acceder 
-        //à des attributs privées 
-/*
-    p3.Nom="Test";
-    p3.Abscisse=3;
-    p3.Ordonnee=4;
-  */
+        /*******************************************************************************************************
+	************************* REPONSE **********************************************************************
+	*Ce code va generer un erreur , car on tente d'acceder 
+         à des attributs privées, il faut utiliser un getter et setter(exemple : getNom ,setAbssice ...) 
+	 pour acceder au attribut privées de classe 
+	 *******************************************************************************************************/ 
+
+
+	/*
+	    p3.Nom="Test";
+	    p3.Abscisse=3;
+	    p3.Ordonnee=4;
+  	*/
         
     }
 }
